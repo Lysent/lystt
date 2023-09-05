a5b422500fb12ea2086990a8ac026b94fe5aa6bf
superwibr <superwibr@gmail.com>
2023-09-04 20:26:17 -0400

    rig events to net

--- src/client/client.js
+++ src/client/client.js
@@ -7,6 +7,9 @@ class LysTClient {
 		this.gs = new GamestateClient({});
 		this.ev = new EventsClient(this.gs);
 		this.net = new NetworkClient(serverurl, {name, key});
+
+		this.net.io.on("players", list => this.ev.setPlayers(list));
+		this.net.io.on("map", mapobj => this.gs.setMap(mapobj));
 	};
 };
 
