528e19865ca0c77d51d9c9dcb1797e8fa590c874
superwibr <superwibr@gmail.com>
2023-10-08 16:53:03 -0400

    purge network use

--- src/client/client.js
+++ src/client/client.js
@@ -2,10 +2,10 @@ import { EventsClient } from "./events.js";
 import { StateClient } from "./state.js";
 
 class LysTClient {
-	constructor(serverurl, { name, key }) {
+	constructor() {
 		this.state = new StateClient();
 		this.ev = new EventsClient(this.state);
 	};
 };
 
-window.lystt = new LysTClient("ws://localhost:3000", {name: "Tikup", key: "supersecurepassword"});
+window.lystt = new LysTClient();
