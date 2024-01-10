8b7abcbd52675738685a058d563e6141a6c064b4
superwibr <superwibr@gmail.com>
2024-01-09 19:36:04 -0500

    begin actual devving

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import { LysTServer } from "./server/index.js";
+import { types } from "./shared/types.js";
+
+const options = {
+	port: 3000,
+	content: {
+		lystt: types
+	}
+}
+
+const game = new LysTServer(options);
+
+process.on('uncaughtException', e => console.error(e));
