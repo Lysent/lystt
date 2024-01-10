8ae95ac01fc735365583e412e90b161da7bbf308
superwibr <superwibr@gmail.com>
2024-01-09 19:31:58 -0500

    ignore old src

--- src_/shared/utils.js
+++
@@ -1,8 +0,0 @@
-export const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
-
-export const inSquare = ([x1, y1], [x2, y2], d) => Math.abs(x1 - x2) <= d && Math.abs(y1 - y2) <= d;
-
-export const buf2hex = buffer =>
-	[...new Uint8Array(buffer)]
-		.map(x => x.toString(16).padStart(2, '0'))
-		.join('');
