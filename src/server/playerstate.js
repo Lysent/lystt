7b370e3f66404cccad3a28f0d98c3b29ec264d43
superwibr <superwibr@gmail.com>
2024-01-11 00:31:49 -0500

    works

--- src/server/playerstate.js
+++ src/server/playerstate.js
@@ -1,3 +1,5 @@
+import * as crypto from "node:crypto";
+
 const
 	buf2hex = buffer =>
 		[...new Uint8Array(buffer)]
@@ -9,16 +11,20 @@ const
 				.map(h => parseInt(h, 16))
 		);
 
-const hashstr = async str => await crypto.subtle.digest('SHA-256', new TextEncoder().encode(str));
+const hashstr = async (str, algo) => await crypto.subtle.digest(algo, new TextEncoder().encode(str));
 
 class Players {
 	constructor(fromobj) {
-		this.players = fromobj || {};
+		const defobj = {
+			system: { online: true, id: "system" }
+		}
+
+		this.players = fromobj ? { ...defobj, ...fromobj } : defobj;
 	}
 
 	async join(uname, pass) {
 		const uobj = this.players[uname];
-		const passhash = buf2hex(await hashstr(pass));
+		const passhash = buf2hex(await hashstr(pass, "SHA-256"));
 
 		// login
 		if (uobj?.pass === passhash) {
@@ -32,20 +38,22 @@ class Players {
 		this.players[uname] = {
 			online: true,
 			pass: passhash,
-			id: buf2hex(await hashstr(`${uname}:${uobj.pass}.${Math.floor(Math.random() * 10 ** 5)}`))
+			id: buf2hex(await hashstr(`${uname}:${passhash}.${Math.floor(Math.random() * 10 ** 5)}`, "SHA-1"))
 		};
-		return;
+		return `Registered as ${uname}`;
 	}
 
 	leave(uname) {
 		const uobj = this.players[uname];
 		if (!uobj) throw "User with that name does not exist";
 		uobj.online = false;
+		return `Player "${uname}" left`;
 	}
 
 	delete(uname) {
 		if (!this.players[uname]) throw "User with that name does not exist";
 		delete this.players[uname];
+		return `Player "${uname}" was deleted`;
 	}
 
 	// get user's id by name
@@ -57,11 +65,11 @@ class Players {
 	}
 
 	// uname from id
-	fromid(id){
-		const idmatches = Object.entries(this.players).filter(([uname, uobj]) => id === uobj.id)[0];
-		if(idmatches.length === 0) throw "Did no get any matches for id";
-		if(idmatches.length > 1) throw "ID is not unique, which is a critical issue!";
-		return idmatches[0];
+	fromid(id) {
+		const idmatches = Object.entries(this.players).filter(([uname, uobj]) => id === uobj.id);
+		if (idmatches.length === 0) throw "Did no get any matches for id";
+		if (idmatches.length > 1) throw "ID is not unique, which is a critical issue!";
+		return idmatches[0][0];
 	}
 
 	playerobj(uname) {
